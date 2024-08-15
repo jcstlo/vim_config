a1c806632e784712a01923fcda78a6e807163e69
Joseph Castillo <joseph.cast0226@gmail.com>
2024-08-14 22:59:01 -0700

    Update completion map to Tab, and update some plugins

--- neovim/nvim/lua/config/lsp.lua
+++ neovim/nvim/lua/config/lsp.lua
@@ -63,7 +63,7 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     }),
 })
 
