d36d8363b5214727c846690dd0e35b09edf7c038
Joseph Castillo <joseph.cast0226@gmail.com>
2024-10-07 17:39:09 -0700

    Update leap/easymotion maps in vim/nvim

--- neovim/nvim/lua/config/leap.lua
+++ neovim/nvim/lua/config/leap.lua
@@ -1,2 +1,7 @@
-vim.keymap.set('n', '<leader>j', '<Plug>(leap)')
-vim.keymap.set('n', '<leader>J', '<Plug>(leap-from-window)')
+require('leap').opts.safe_labels = {}
+require('leap').opts.preview_filter = function () return false end
+
+vim.keymap.set('n', 's', '<Plug>(leap)')
+vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+vim.keymap.set({'x', 'o'}, 's', '<Plug>(leap-forward)')
+vim.keymap.set({'x', 'o'}, 'S', '<Plug>(leap-backward)')
